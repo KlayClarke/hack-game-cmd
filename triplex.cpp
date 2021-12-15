e0d5077f59330bfd2a2bb88e612c38d73bd38c54
Klay Clarke <klayaclarke@gmail.com>
2021-12-15 09:48:14 -0500

    initialize integer variables and print sum of

--- triplex.cpp
+++ triplex.cpp
@@ -5,5 +5,13 @@ int main()
   std::cout << "You are attempting to break out of political prison and expose secrets.";
   std::cout << std::endl;
   std::cout << "You need to enter the correct codes to continue...";
+
+  int a = 4;
+  int b = 8;
+  int c = 12;
+
+  std::cout << std::endl;
+  std::cout << a + b + c;
+
   return 0;
 }
